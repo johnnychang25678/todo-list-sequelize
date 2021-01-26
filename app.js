ddfc046587877e0ab76a90e0a0f528f1f5cd8dce
Johnny Chang <zjps3407@gmail.com>
2021-01-26 11:31:10 +0800

    add views

--- app.js
+++ app.js
@@ -8,11 +8,34 @@ const PORT = 3000
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
+
+// middlewares
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
+// routes
 app.get('/', (req, res) => {
   res.send('hello world')
+})
+
+app.get('/users/login', (req, res) => {
+  res.render('login')
+})
+
+app.post('/users/login', (req, res) => {
+  res.send('login')
+})
+
+app.get('/users/register', (req, res) => {
+  res.render('register')
+})
+
+app.post('/users/register', (req, res) => {
+  res.send('register')
+})
+
+app.get('/users/logout', (req, res) => {
+  res.send('logout')
 })
 
 app.listen(PORT, () => {
