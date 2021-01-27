4deff75ba569c2f2a5220082705baf5a64e9edb9
Johnny Chang <zjps3407@gmail.com>
2021-01-27 10:01:43 +0800

    add passport authentication

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
 const bcrypt = require('bcryptjs')
+const session = require('express-session')
+const usePassport = require('./config/passport')
+const passport = require('passport')
 
 const app = express()
 const PORT = 3000
@@ -16,6 +19,13 @@ app.set('view engine', 'hbs')
 // middlewares
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+app.use(session({
+  secret: 'ThisIsMySecret',
+  resave: false,
+  saveUninitialized: true
+}))
+usePassport(app)
+
 
 // routes
 app.get('/', (req, res) => {
@@ -38,9 +48,10 @@ app.get('/users/login', (req, res) => {
   res.render('login')
 })
 
-app.post('/users/login', (req, res) => {
-  res.send('login')
-})
+app.post('/users/login', passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/users/login'
+}))
 
 // register
 app.get('/users/register', (req, res) => {
